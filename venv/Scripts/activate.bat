ca02ab50620e0651bd531da9c9fe2ea4a86ecc66
AlexYourBI <alexlourenc@gmail.com>
2026-02-27 21:46:35 -0300

    refactor: stabilize python environment to 3.12 and update documentation

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alex Lourenço\Desktop\PROJETO_ENGENHARIA\project-root\venv
+set VIRTUAL_ENV=C:\Users\Alex Lourenço\Desktop\project-root\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
